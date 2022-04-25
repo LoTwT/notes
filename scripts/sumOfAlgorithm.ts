8eadc7620739521e9aa88e08e80c5e4a0f89e47d
LoTwT <709937065@qq.com>
2022-04-25 08:21:53 +0800

    chore: add a script to calc the sum of algorithms

--- scripts/sumOfAlgorithm.ts
+++ scripts/sumOfAlgorithm.ts
@@ -1,1 +1,15 @@
-import {} from "fs"
+import { readdir } from "fs/promises"
+import { resolve } from "path"
+
+import { createCommonJS } from "mlly"
+
+const { __dirname } = createCommonJS(import.meta.url)
+
+const targetPath = resolve(__dirname, "../docs/zh/dataAndAlgorithm/algorithm")
+const files = await readdir(targetPath)
+
+const reg = /^\d{0,}-.*.md$/
+
+const len = files.filter((f) => reg.test(f)).length
+
+console.log(`已经刷了 ${len} 题。`)
